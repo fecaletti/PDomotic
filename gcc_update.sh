497a6f42cc880c69bfdd4050124af3990d80617e
fecaletti <fe.caletti@gmail.com>
2021-11-26 21:56:11 -0300

    s

--- gcc_update.sh
+++ gcc_update.sh
@@ -6,4 +6,4 @@ sudo ln -s /usr/include/arm-linux-gnueabihf/gnu /usr/include/gnu &&
 sudo ln -s /usr/include/arm-linux-gnueabihf/asm /usr/include/asm &&
 sudo ln -s /usr/lib/arm-linux-gnueabihf/crti.o /usr/lib/crti.o &&
 sudo ln -s /usr/lib/arm-linux-gnueabihf/crt1.o /usr/lib/crt1.o &&
-sudo ln -s /usr/lib/arm-linux-gnueabihf/crtn.o /usr/lib/crtn.o &&
+sudo ln -s /usr/lib/arm-linux-gnueabihf/crtn.o /usr/lib/crtn.o
